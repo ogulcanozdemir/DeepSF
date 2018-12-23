5edc2b95e9ea8142c483b737d3d3de9a4c50deed
Oğulcan ÖZDEMİR <ogulcan.ozdemir@yahoo.com>
2018-12-23 16:31:05 +0300

    Update predict_main.py

--- training/predict_main.py
+++ training/predict_main.py
@@ -3,7 +3,8 @@ import sys
 import os
 import shutil
 
-GLOBAL_PATH='/home/casp13/deepsf_3d/Github/test/DeepSF/';
+GLOBAL_PATH='/media/pilab/ssd_data/test/DeepSF/';
+PYTHON_PATH='/media/pilab/ssd_data/test/miniconda2/envs/theano/bin/python2.7';
 sys.path.insert(0, GLOBAL_PATH+'/lib')
 
 
@@ -67,15 +68,12 @@ if not os.path.exists(weightfile):
   print "Cuoldn't find file ",modelfile
   exit(-1)
 
-
-
-
 print "###### Evaluating Training data";
-cmd1='python '+ GLOBAL_PATH + '/lib/DLS2F_predict_fea.py  '+ train_datafile + '  ' + modelfile+ '  ' + weightfile+ '  ' + feature_dir_global + '  ' +  pssm_dir_global + ' '   + resultdir + '  '+str(ktop_node)
+cmd1=PYTHON_PATH+' '+ GLOBAL_PATH + '/lib/DLS2F_predict_fea.py  '+ train_datafile + '  ' + modelfile+ '  ' + weightfile+ '  ' + feature_dir_global + '  ' +  pssm_dir_global + ' '   + resultdir + '  '+str(ktop_node)
 print "Running ", cmd1,"\n\n"
 os.system(cmd1)
 
-cmd2='python '+ GLOBAL_PATH + '/lib/DLS2F_evaluate_SCOP.py  '+ train_datafile  + '  '+GLOBAL_PATH +'/datasets/D1_SimilarityReduction_dataset/fold_label_relation2.txt  '  + resultdir + '  ' + results_train
+cmd2=PYTHON_PATH+' '+ GLOBAL_PATH + '/lib/DLS2F_evaluate_SCOP.py  '+ train_datafile  + '  '+GLOBAL_PATH +'/datasets/D1_SimilarityReduction_dataset/fold_label_relation2.txt  '  + resultdir + '  ' + results_train
 print "Running ", cmd2,"\n\n"
 os.system(cmd2)
 
@@ -86,11 +84,11 @@ if not os.path.exists(resultdir):
     os.makedirs(resultdir)
 
 print "###### Evaluating Testing data";
-cmd1='python '+ GLOBAL_PATH + '/lib/DLS2F_predict_fea.py  '+ test_datafile + '  ' + modelfile+ '  ' + weightfile + '  ' +  feature_dir_global + '  ' +  pssm_dir_global + ' '   + resultdir + '   '+str(ktop_node)
+cmd1=PYTHON_PATH+' '+ GLOBAL_PATH + '/lib/DLS2F_predict_fea.py  '+ test_datafile + '  ' + modelfile+ '  ' + weightfile + '  ' +  feature_dir_global + '  ' +  pssm_dir_global + ' '   + resultdir + '   '+str(ktop_node)
 print "Running ", cmd1,"\n\n"
 os.system(cmd1)
 
-cmd2='python '+ GLOBAL_PATH + '/lib/DLS2F_evaluate_SCOP.py  '+ test_datafile  + '  '+GLOBAL_PATH +'/datasets/D1_SimilarityReduction_dataset/fold_label_relation2.txt  '  + resultdir + '  ' + results_test
+cmd2=PYTHON_PATH+' '+ GLOBAL_PATH + '/lib/DLS2F_evaluate_SCOP.py  '+ test_datafile  + '  '+GLOBAL_PATH +'/datasets/D1_SimilarityReduction_dataset/fold_label_relation2.txt  '  + resultdir + '  ' + results_test
 print "Running ", cmd2,"\n\n"
 os.system(cmd2)
 
@@ -100,11 +98,11 @@ if not os.path.exists(resultdir):
     os.makedirs(resultdir)
 
 print "###### Evaluating Validation data";
-cmd1='python '+ GLOBAL_PATH + '/lib/DLS2F_predict_fea.py  '+ val_datafile + '  ' + modelfile+ '  ' + weightfile+ '  ' + feature_dir_global + '  ' +  pssm_dir_global + ' '   + resultdir + '   '+str(ktop_node)
+cmd1=PYTHON_PATH+' '+ GLOBAL_PATH + '/lib/DLS2F_predict_fea.py  '+ val_datafile + '  ' + modelfile+ '  ' + weightfile+ '  ' + feature_dir_global + '  ' +  pssm_dir_global + ' '   + resultdir + '   '+str(ktop_node)
 print "Running ", cmd1,"\n\n"
 os.system(cmd1)
 
-cmd2='python '+ GLOBAL_PATH + '/lib/DLS2F_evaluate_SCOP.py  '+ val_datafile  + '  '+GLOBAL_PATH +'/datasets/D1_SimilarityReduction_dataset/fold_label_relation2.txt '  + resultdir + '  ' + results_val
+cmd2=PYTHON_PATH+' '+ GLOBAL_PATH + '/lib/DLS2F_evaluate_SCOP.py  '+ val_datafile  + '  '+GLOBAL_PATH +'/datasets/D1_SimilarityReduction_dataset/fold_label_relation2.txt '  + resultdir + '  ' + results_val
 print "Running ", cmd2,"\n\n"
 os.system(cmd2)
 
